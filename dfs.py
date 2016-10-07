424907b23ae0c02f0de109894c3742df12672e90
arsh23 <programmer.arsh@gmail.com>
2016-10-07 18:47:00 +0530

    added dfs

--- dfs.py
+++ dfs.py
@@ -11,15 +11,32 @@ class Graph():
     def display(self):
         print self._graph
 
-    # def dfs_stack(graph, start):
-    #     pass
+    def dfs(self, start, item, visited=None, path=''):
+        if start not in self._graph:
+            raise Exception('Node not found in graph')
+        if visited is None:
+            visited = set()
+        if path == '':
+            path = str(start)
+        else:
+            path += '->' + str(start)
+
+        visited.add(start)
+        if start == item:
+            print path
+            return
+        for x in self._graph[start]:
+            if x not in visited:
+                self.dfs(x, item, visited, path)
+
 
 g = Graph()
 g.add_node('a', ('b', 'd', 's'))
 g.add_node('b', ('a', 'c', 's'))
 g.add_node('c', ('b', 'e'))
 g.add_node('d', ('a', 'g'))
-g.add_node('e', ('c'))
+g.add_node('e', ('c', 'g'))
 g.add_node('g', ('d'))
 g.add_node('s', ('a', 'b'))
-g.display()
+# g.display()
+g.dfs('s', 'g')
